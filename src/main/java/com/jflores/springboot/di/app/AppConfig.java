bcbb0b320423994a9835e26a149ec3b62b8bd4bc
jose-1991 <jose.floresc.java@gmail.com>
2023-08-09 19:15:51 -0400

    anotaciones basicas spring boot

---
+++ src/main/java/com/jflores/springboot/di/app/AppConfig.java
@@ -0,0 +1,41 @@
+package com.jflores.springboot.di.app;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import com.jflores.springboot.di.app.models.domain.ItemFactura;
+import com.jflores.springboot.di.app.models.domain.Producto;
+import com.jflores.springboot.di.app.models.service.IServicio;
+import com.jflores.springboot.di.app.models.service.MiServicio;
+import com.jflores.springboot.di.app.models.service.MiServicioComplejo;
+
+@Configuration
+public class AppConfig {
+
+	@Bean("miServicioSimple")
+	public IServicio registrarMiServicio() {
+		return new MiServicio();
+	}
+	
+	@Bean("miServicioComplejo")
+	@Primary
+	public IServicio registrarMiServicioComplejo() {
+		return new MiServicioComplejo();
+	}
+	
+	@Bean("itemsFactura")
+	public List<ItemFactura> registrarItems(){
+		Producto producto1 = new Producto("Camara Sony", 100);
+		Producto producto2 = new Producto("Bicicleta aro 26", 200);
+		
+		ItemFactura linea1 = new ItemFactura(producto1, 2);
+		ItemFactura linea2 = new ItemFactura(producto2, 4);
+		
+		return Arrays.asList(linea1, linea2);
+
+	}
+}
---
+++ src/main/java/com/jflores/springboot/di/app/models/service/IServicio.java
@@ -0,0 +1,6 @@
+package com.jflores.springboot.di.app.models.service;
+
+public interface IServicio {
+	
+	public String operacion();
+}
